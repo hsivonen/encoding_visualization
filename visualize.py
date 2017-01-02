de177c7d0d3ca6ba62cc2d53069d8e0792e139ac
Henri Sivonen <hsivonen@hsivonen.fi>
2017-01-02 09:40:09 +0200

    Add borders for duplicate code points and Compatibility Ideographs.

--- visualize.py
+++ visualize.py
@@ -48,9 +48,21 @@ def classify(code_point):
         raise Error()
     if code_point < 0x800:
         return "mid"
-    if code_point < 0x10000:
-        return "upper"
-    return "astral"
+    if code_point > 0xFFFF:
+        return "astral"
+    if code_point >= 0xE000 and code_point <= 0xF8FF:
+        return "pua"
+    return "upper"
+
+def check_duplicate(code_point, pointer, index):
+    if code_point in index[:pointer]:
+        return " duplicate"
+    return ""
+
+def check_compatibility(code_point):
+    if code_point >= 0xF900 and code_point <= 0xFAFF:
+        return " compatibility"
+    return ""
 
 def format_index(name, row_length, lang):
     out_file = open("%s.html" % name, "w")
@@ -65,16 +77,15 @@ def format_index(name, row_length, lang):
     new_row = True
     pointer = 0
     row_num = 0
-    for code_point in indexes[name]:
+    index = indexes[name]
+    for code_point in index:
         if new_row:
             out_file.write("<tr><th>%02d<th>%02X" % (row_num, row_num))
             new_row = False
         if not code_point:
             out_file.write((u"<td class=unmapped title='%d = 0x%X'>\uFFFD" % (pointer, pointer)).encode("utf-8"))
-        elif previous and previous + 1 == code_point:
-            out_file.write((u"<td class='contiguous %s' title='U+%04X, %d = 0x%X'>%s" % (classify(code_point), code_point, pointer, pointer, unichr(code_point))).encode("utf-8"))
         else:
-            out_file.write((u"<td class='discontiguous %s' title='U+%04X, %d = 0x%X'>%s" % (classify(code_point), code_point, pointer, pointer, unichr(code_point))).encode("utf-8"))
+            out_file.write((u"<td class='%s %s%s%s' title='U+%04X, %d = 0x%X'>%s" % ("contiguous" if previous and previous + 1 == code_point else "discontiguous", classify(code_point), check_duplicate(code_point, pointer, index), check_compatibility(code_point), code_point, pointer, pointer, unichr(code_point))).encode("utf-8"))
         previous = code_point
         pointer += 1
         if pointer % row_length == 0:
@@ -93,10 +104,14 @@ index_file.write("""<!DOCTYPE html><html lang=en><meta charset=utf-8><title>Enco
     <li class=unmapped>Unmapped
     <li class=mid>Two bytes in UTF-8
     <li class="mid contiguous">Two bytes in UTF-8, code point follows immediately the code point of previous pointer
-    <li class=upper>Three bytes in UTF-8
-    <li class="upper contiguous">Three bytes in UTF-8, code point follows immediately the code point of previous pointer
+    <li class=upper>Three bytes in UTF-8 (non-PUA)
+    <li class="upper contiguous">Three bytes in UTF-8 (non-PUA), code point follows immediately the code point of previous pointer
+    <li class=pua>Private Use
+    <li class="pua contiguous">Private Use, code point follows immediately the code point of previous pointer
     <li class=astral>Four bytes in UTF-8
     <li class="astral contiguous">Four bytes in UTF-8, code point follows immediately the code point of previous pointer
+    <li class=duplicate>Duplicate code point already mapped at an earlier index
+    <li class=compatibility>CJK Compatibility Ideograph
     </ul>
     <h2>The Indices</h2>
     <ul>""");
